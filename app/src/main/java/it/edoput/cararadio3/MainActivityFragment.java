6d103a6c7fbdadf490b56631fe09583fab5a3c95
Edoardo Putti <edoardo.putti@gmail.com>
2016-03-19 21:36:43 +0100

    listview is now working

--- app/src/main/java/it/edoput/cararadio3/MainActivityFragment.java
+++ app/src/main/java/it/edoput/cararadio3/MainActivityFragment.java
@@ -1,24 +1,31 @@
 package it.edoput.cararadio3;
 
-import android.net.Uri;
+import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
-import android.util.Xml;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,22 +36,24 @@ public class MainActivityFragment extends Fragment {
     // the fragment list view to show messages
     private ListView listView;
 
-    // an adapter
-    // private CustomAdapter mAdapter;
+    // a subclass of ListAdapter
+    private CustomAdapter mAdapter;
 
     //
-    // private ArrayList<Message> messageList = new ArrayList<Message>();
+    private ArrayList<TextMessage> messageList = new ArrayList<TextMessage>();
 
     // hold the swiperefreshlayout reference
-    // must implement `setOnRefreshListener` to listen
-    // for swipe event
     private SwipeRefreshLayout refreshLayout;
 
     // Fragment constructor
     public MainActivityFragment() {
     }
 
-    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -59,147 +68,168 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onRefresh() {
                 refreshLayout.setRefreshing(true);
-//                new LoadMessageTask().execute();
+                messageList.clear();
+                new LoadMessageTask().execute();
             }
         });
 
+        new LoadMessageTask().execute();
+
+        mAdapter = new CustomAdapter(getActivity(), messageList);
+
         // set global reference to ListView in fragment
         listView = (ListView) view.findViewById(R.id.list);
+        View emptyView = view.findViewById(R.id.empty);
+
+        listView.setAdapter(mAdapter);
+        listView.setEmptyView(emptyView);
 
         // return reference to fragment view
         return view;
     }
 
+    // implement async task to get text messages from the internet
+    private class LoadMessageTask extends AsyncTask<Void, Void, List<TextMessage>> {
+        String uristring = "http://www.radio3.rai.it/radio3sms/smsradiotre.xml";
+        InputStream response;
+        TextMessageList textMessageList;
+        @Override
+        protected List<TextMessage> doInBackground (Void ... params) {
+            try {
+                // receive xml
+                response = downloadUrl(uristring);
+                Serializer serializer = new Persister();
+                textMessageList = serializer.read(TextMessageList.class, response);
+
+            } catch (IOException ioexception) {
+                Log.e("SimpleXML", "Error reading response", ioexception);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+            }
+            return textMessageList.getSms();
+        }
+
+        private InputStream downloadUrl(String uristring) throws IOException {
+            URL url = new URL(uristring);
+            // the connection used to request data from the net
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            return conn.getInputStream();
+        }
+
+        @Override
+        protected void onPostExecute(List<TextMessage> textMessages) {
+            messageList = (ArrayList<TextMessage>) textMessages;
+            mAdapter = new CustomAdapter(getActivity(), messageList);
+            listView.setAdapter(mAdapter);
+            refreshLayout.setRefreshing(false);
+        }
+    }
+
 }
 
-// implement async task to get text messages from the internet
-// class LoadMessageTask extends AsyncTask<Void, Void, String> {
-//     String uristring = "http://www.radio3.rai.it/radio3sms/smsradiotre.xml";
-//     @Override
-//     protected String doInBackground (Void ... params) {
-//         try {
-//             // receive xml
-//             // publish to ui with publishProgress
-//             loadXmlFromNetwork(uristring);
-//         } catch (IOException ioexception) {
-//             return getResources().getString(R.string.connection_error);
-//         } catch (XmlPullParserException xmlexception) {
-//             return getResources().getString(R.string.xml_error);
-//         }
-//         return null;
-//     }
+class CustomAdapter extends BaseAdapter {
+    Activity activity;
+    LayoutInflater inflater;
+    List<TextMessage> list;
 
-//     private String loadXmlFromNetwork(String uristring) throws XmlPullParserException, IOException {
-//         InputStream in = null;
-//         TextMessageXmlParser textMessageXmlParser = new TextMessageXmlParser();
-//         List<TextMessage> messsages = null;
+    public CustomAdapter (Activity activity, List<TextMessage> list) {
+        this.activity = activity;
+        this.list = list;
+    }
 
-//     }
+    @Override
+    public int getCount() {
+        return this.list.size();
+    }
 
-//     private InputStream downloadUrl(String uristring) throws IOException {
-//         URL url = new URL(uristring);
-//         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//         conn.setReadTimeout(10000;
-//         conn.setConnectTimeout(15000;
-//         conn.setRequestMethod("GET");
-//         conn.setDoInput(true);
-//         conn.connect();
-//         return conn.getInputStream();
-//  }
+    @Override
+    public Object getItem(int position) {
+        return this.list.get(position);
+    }
 
-//     private void onPostExecute() {
-//     }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-// }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (inflater == null) {
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.layout_row, null);
+        }
 
-// class TextMessageXmlParser {
-//     // namespace not setted, required for following xml tags
-//     private static final String ns = null;
+        TextView msgtesto = (TextView) convertView.findViewById(R.id.msgtesto);
+        TextView msgdata  = (TextView) convertView.findViewById(R.id.msgdata);
+        TextView msgora   = (TextView) convertView.findViewById(R.id.msgora);
 
-//     public List parse(InputStream in) throws XmlPullParserException, IOException{
-//         try {
-//             XmlPullParser parser = Xml.newPullParser();
-//             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-//             parser.setInput(in, null);
-//             parser.nextTag();
-//             return readFeed(parser);
-//         }
-//         finally {
-//             in.close();
-//         }
-//     }
+        TextMessage message = list.get(position);
+        msgtesto.setText(message.getMsgtesto());
+        msgdata.setText(message.getMsgdata());
+        msgora.setText(message.getMsgora());
 
-//     private List readFeed (XmlPullParser parser) throws XmlPullParserException, IOException {
-//         List entries = new ArrayList();
+        return convertView;
 
-//         parser.require(XmlPullParser.START_TAG, ns, "smsradiotre");
-//         while(parser.next() != XmlPullParser.END_TAG) {
-//             if (parser.getEventType() != XmlPullParser.START_TAG) {
-//                 continue;
-//             }
+    }
+}
 
-//             String name = parser.getName();
-//             if (name.equals("sms")) {
-//                 entries.add(readTextMessage(parser));
-//             } else {
-//                 skip(parser);
-//             }
-//         }
 
-//         return entries;
-//     }
 
-//     private TextMessage readTextMessage(XmlPullParser parser) throws XmlPullParserException, IOException {
-//         parser.require(XmlPullParser.START_TAG, ns, "sms");
-//         String message = null;
-//         while (parser.next() != XmlPullParser.END_TAG) {
-//             if (parser.getEventType() != XmlPullParser.START_TAG) {
-//                 continue;
-//             }
-//             String name = parser.getName();
-//             switch (name) {
-//                 case "msgtesto":
-//                     message = readMessage(parser);
-//                     break;
-//             }
-//         }
-//         return new TextMessage(message);
-//     }
+/*
+The XML tree is shaped like this
 
-//     private String readMessage (XmlPullParser parser) throws XmlPullParserException, IOException {
-//         parser.require(XmlPullParser.START_TAG, ns, "msgtesto");
-//         String result = "";
-//         if (parser.next() == XmlPullParser.TEXT) {
-//             result = parser.getText();
-//             parser.nextTag();
-//         }
-//         return result;
-//     }
+<smsradiotre>
+    <sms>
+        <msgdata></msgdata>
+        <msgora></msgora>
+        <programma></programma>
+        <msgtesto></msgtesto>
+    </sms>
+</smsradiotre>
+*/
 
-//     private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
-//         if (parser.getEventType() != XmlPullParser.START_TAG) {
-//             throw new IllegalStateException();
-//         }
-//         int depth = 1;
+@Root(name = "smsradiotre")
+class TextMessageList {
+    @ElementList(inline = true)
+    private List<TextMessage> sms;
 
-//         while (depth != 0) {
-//             switch (parser.next()) {
-//                 case XmlPullParser.END_TAG:
-//                     depth--;
-//                     break;
-//                 case XmlPullParser.START_TAG:
-//                     depth++;
-//                     break;
-//             }
-//         }
-//     }
-// }
+    public List<TextMessage> getSms() {
+        return this.sms;
+    }
+}
+@Root(name = "sms")
+class TextMessage {
+    @Element(name = "msgdata")
+    public String msgdata;
 
-// class TextMessage {
-//     public final String message;
+    @Element(name = "msgora")
+    public String msgora;
 
-//     private TextMessage(String message) {
-//         this.message = message;
-//     }
-// }
+    @Element(name = "programma")
+    public String programma;
+
+    @Element(name = "msgtesto")
+    public String msgtesto;
+
+    public String getMsgdata() {
+        return msgdata;
+    }
+
+    public String getMsgora() {
+        return msgora;
+    }
+
+    public String getMsgtesto() {
+        return msgtesto;
+    }
+}
+
 
